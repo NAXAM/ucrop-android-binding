9675054abaa9ba6c1982a0fcf952631d3a0fccce
DucIT <DucIT@LAPTOP>
2017-06-13 21:47:20 +0700

    [Naxam][uCrop-Droid] : Binding the library

---
+++ Naxam.uCrop.Droid/Additions/BitmapLoadTask.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Yalantis.Ucrop.Task
+{
+    public partial class BitmapLoadTask : global::Android.OS.AsyncTask
+    {
+        protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] @params)
+        {
+            // 
+            return null;
+        }
+    }
+}
--- demo/uCropQs/MainActivity.cs
+++ demo/uCropQs/MainActivity.cs
@@ -1,18 +1,40 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
+using Java.Util;
+using Com.Yalantis.Ucrop;
+using Android.Support.V7.App;
 
 namespace uCropQs
 {
     [Activity(Label = "uCropQs", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private Button btnCrop;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
+
+            btnCrop = FindViewById<Button>(Resource.Id.btnCrop);
+
+            btnCrop.Click += BtnCrop_Click;
+        }
+
+        private void BtnCrop_Click(object sender, System.EventArgs e)
+        {
+            string path = Android.OS.Environment.ExternalStorageDirectory.Path;
+
+            UCrop uCrop = UCrop.Of(Android.Net.Uri.Parse(String.Format(Locale.Default.ToString(), "https://unsplash.it/500/500/?random")),
+                Android.Net.Uri.FromFile(new Java.IO.File(path, "SAMPLE_CROPPED_IMAGE_NAME.png")));
+
+            uCrop.UseSourceImageAspectRatio();
+            uCrop.WithAspectRatio(1, 1);
+
+            uCrop.Start(this);
         }
     }
 }
